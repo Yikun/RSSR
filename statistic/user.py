1c161cf17b22175f7688d993c0b6a4b26cc76f2e
Yikun Jiang <yikunkero@gmail.com>
2014-12-02 23:42:03 +0800

    Add user load

--- statistic/user.py
+++ statistic/user.py
@@ -1,5 +1,20 @@
+import csv
+
+
 class User(object):
-    pass
+
+    def __init__(self, path):
+        self.path = path
+        self.u_list = []
+        self.load(path)
+
+    def load(self, path):
+        with open(path, 'r') as infile:
+            next(infile)
+            reader = csv.reader(infile)
+            for user in reader:
+                self.u_list.append(user)
 
 if __name__ == '__main__':
-    pass
+    u = User("../data/test/user.csv")
+    print u.u_list
