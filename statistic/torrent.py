c85040445fbb46248dc554160e40327bbe8c2386
Jiang Yikun <yikunkero@gmail.com>
2014-12-03 21:26:22 +0800

    Add Torrent class statistic api and visualize.

--- statistic/torrent.py
+++ statistic/torrent.py
@@ -1,5 +1,85 @@
+import csv
+import operator
+
+
 class Torrent(object):
-    pass
+
+    def __init__(self, path):
+        self.path = path
+        self.t_list = []
+        self.load(path)
+
+    def load(self, path):
+        with open(path, 'r') as infile:
+            next(infile)
+            reader = csv.reader(infile)
+            for t in reader:
+                self.t_list.append([int(t[0]),
+                                    t[1],
+                                    int(t[2]),
+                                    int(t[3]),
+                                    int(t[4]),
+                                    int(t[5]),
+                                    int(t[6]),
+                                    int(t[7]),
+                                    int(t[8])
+                                    ])
+
+    def top(self, rank, keyindex):
+        topx = sorted(
+            self.t_list, key=operator.itemgetter(keyindex), reverse=True)
+        return topx[0:rank]
+
+    def cids(self, cid):
+        dic = {}
+        downloads = []
+        for t in self.t_list:
+            if t[2] in cid:
+                if t[2] in dic:
+                    dic[t[2]] += 1
+                else:
+                    dic[t[2]] = 1
+        for c in cid:
+            if c in dic:
+                downloads.append(dic[c])
+            else:
+                downloads.append(0)
+
+        return downloads  # sorted(dic.items(), key=operator.itemgetter(0))
+
+    def allcidnum(self):
+        return t.cids([1000, 1002, 1003, 1004, 1005,
+                       1200, 1201, 1202, 1203, 1204, 1205, 1206, 1207,
+                       1400, 1401, 1402, 1403, 1404, 1405, 1406,
+                       1600, 1601, 1602, 1603, 1604,
+                       1800, 1801, 1802, 1803, 1804,
+                       2000, 2001, 2002, 2003, 2004,
+                       2200, 2201, 2202, 2203,
+                       2400, 2401, 2402, 2403, 2404,
+                       2600, 2601, 2602, 2603, 2604,
+                       2800, 2801, 2802, 2803, 2804,
+                       3000, 3001, 3002])
+
+    def topcidnum(self):
+        return [sum(self.cids([1000, 1002, 1003, 1004, 1005])),
+                sum(self.cids(
+                    [1200, 1201, 1202, 1203, 1204, 1205, 1206, 1207])),
+                sum(self.cids([1400, 1401, 1402, 1403, 1404, 1405, 1406])),
+                sum(self.cids([1600, 1601, 1602, 1603, 1604])),
+                sum(self.cids([1800, 1801, 1802, 1803, 1804])),
+                sum(self.cids([2000, 2001, 2002, 2003, 2004])),
+                sum(self.cids([2200, 2201, 2202, 2203])),
+                sum(self.cids([2400, 2401, 2402, 2403, 2404])),
+                sum(self.cids([2600, 2601, 2602, 2603, 2604])),
+                sum(self.cids([2800, 2801, 2802, 2803, 2804])),
+                sum(self.cids([3000, 3001, 3002]))
+                ]
 
 if __name__ == '__main__':
-    pass
+    t = Torrent("../data/torrent.csv")
+    # id(0),filename(1),cid(2),size(3),numfiles(4),finished(5),lasttime(6),lastuser(7),tid(8)
+    for x in t.top(20, 5):
+        print x[1], x[5]
+
+    print t.allcidnum()
+    print t.topcidnum()
--- statistic/visualize.py
+++ statistic/visualize.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 import pygal
 import history
+import torrent
 
 from pygal.style import Style
-
-hsvgpath = "../view/history/"
-
-h = history.History("../data/history.dat")
 
 custom_style = Style(
     background='white',
@@ -28,74 +25,116 @@ custom_style = Style(
             'rgb(242, 156, 156)')
 )
 
-line = pygal.Line(
-    width=800, height=400, interpolate='hermite', style=custom_style)
-line.title = u'每月种子下载平均量统计'
-line.x_labels = [u'1月', u'2月', u'3月', u'4月', u'5月',
-                 u'6月', u'7月', u'8月', u'9月', u'10月', u'11月', u'12月']
-line.add(u'2012年', h.monthes(2012, None))
-line.add(u'2013年', h.monthes(2013, None))
-line.add(u'2014年', h.monthes(2014, None))
-line.render_to_file(hsvgpath + 'month_ave.svg')
 
-line = pygal.Line(
-    width=800, height=400, interpolate='hermite', style=custom_style)
-line.title = u'每月种子下载累积量统计'
-line.x_labels = [u'起始', u'1月', u'2月', u'3月', u'4月', u'5月',
-                 u'6月', u'7月', u'8月', u'9月', u'10月', u'11月', u'12月']
-line.add(u'2012年', h.accum(h.monthes(2012)))
-line.add(u'2013年', h.accum(h.monthes(2013)))
-line.add(u'2014年', h.accum(h.monthes(2014)))
-line.render_to_file(hsvgpath + 'month_all.svg')
+def historysvg():
+    hsvgpath = "../view/history/"
 
-line = pygal.Line(
-    width=800, height=400, interpolate='hermite', style=custom_style, dots_size=1)
-line.title = u'2012年每月下载量统计'
-line.x_labels = [u'上旬', u'中旬', u'下旬']
-for month in range(1, 13):
-    line.add(u'2012年%02d月' % month, h.accum(h.days(2012, month)))
-line.render_to_file(hsvgpath + '2012_day_add.svg')
+    h = history.History("../data/history.dat")
 
-line = pygal.Line(
-    width=800, height=400, interpolate='hermite', style=custom_style, dots_size=1)
-line.title = u'2013年每月下载量统计'
-line.x_labels = [u'上旬', u'中旬', u'下旬']
-for month in range(1, 13):
-    line.add(u'2013年%02d月' % month, h.accum(h.days(2013, month)))
-line.render_to_file(hsvgpath + '2013_day_add.svg')
+    line = pygal.Line(
+        width=800, height=400, interpolate='hermite', style=custom_style)
+    line.title = u'每月种子下载平均量统计'
+    line.x_labels = [u'1月', u'2月', u'3月', u'4月', u'5月',
+                     u'6月', u'7月', u'8月', u'9月', u'10月', u'11月', u'12月']
+    line.add(u'2012年', h.monthes(2012, None))
+    line.add(u'2013年', h.monthes(2013, None))
+    line.add(u'2014年', h.monthes(2014, None))
+    line.render_to_file(hsvgpath + 'month_ave.svg')
 
-line = pygal.Line(
-    width=800, height=400, interpolate='hermite', style=custom_style, dots_size=1)
-line.title = u'2014年每月下载量统计'
-line.x_labels = [u'上旬', u'中旬', u'下旬']
-for month in range(1, 13):
-    line.add(u'2014年%02d月' % month, h.accum(h.days(2014, month)))
-line.render_to_file(hsvgpath + '2014_day_add.svg')
+    line = pygal.Line(
+        width=800, height=400, interpolate='hermite', style=custom_style)
+    line.title = u'每月种子下载累积量统计'
+    line.x_labels = [u'起始', u'1月', u'2月', u'3月', u'4月', u'5月',
+                     u'6月', u'7月', u'8月', u'9月', u'10月', u'11月', u'12月']
+    line.add(u'2012年', h.accum(h.monthes(2012)))
+    line.add(u'2013年', h.accum(h.monthes(2013)))
+    line.add(u'2014年', h.accum(h.monthes(2014)))
+    line.render_to_file(hsvgpath + 'month_all.svg')
 
-line = pygal.Line(
-    width=800, height=400, interpolate='hermite', style=custom_style, dots_size=1)
-line.title = u'2014年10~11月每日下载量统计'
-line.x_labels = [u'上旬', u'中旬', u'下旬']
-line.add(u'2014年10月', h.days(2014, 10))
-line.add(u'2014年11月', h.days(2014, 11))
-line.render_to_file(hsvgpath + '2014_11mon.svg')
+    line = pygal.Line(
+        width=800, height=400, interpolate='hermite', style=custom_style, dots_size=1)
+    line.title = u'2012年每月下载量统计'
+    line.x_labels = [u'上旬', u'中旬', u'下旬']
+    for month in range(1, 13):
+        line.add(u'2012年%02d月' % month, h.accum(h.days(2012, month)))
+    line.render_to_file(hsvgpath + '2012_day_add.svg')
 
-line = pygal.Line(
-    width=800, height=400, interpolate='hermite', style=custom_style, dots_size=1)
-line.title = u'2014年11月最近几周每天下载量统计'
-line.x_labels = []
-for hours in range(1, 8):
-    line.x_labels.append(u'周%d' % hours)
-for week in range(40, 47):
-    line.add(u'2014年 第%d周' % week, h.weekdays(2014, week))
-line.render_to_file(hsvgpath + '2014_week.svg')
+    line = pygal.Line(
+        width=800, height=400, interpolate='hermite', style=custom_style, dots_size=1)
+    line.title = u'2013年每月下载量统计'
+    line.x_labels = [u'上旬', u'中旬', u'下旬']
+    for month in range(1, 13):
+        line.add(u'2013年%02d月' % month, h.accum(h.days(2013, month)))
+    line.render_to_file(hsvgpath + '2013_day_add.svg')
 
-line = pygal.Line(
-    width=800, height=400, interpolate='hermite', style=custom_style, dots_size=1)
-line.title = u'2014年11月15~22日每小时下载量统计'
-line.x_labels = []
-for hours in range(0, 24):
-    line.x_labels.append(u'%02d点' % hours)
-for day in range(15, 22):
-    line.add(u'2014年11月%02d日 周%02d' % (day, day - 14), h.hours(2014, 11, day))
-line.render_to_file(hsvgpath + '2014_11_hours_add.svg')
+    line = pygal.Line(
+        width=800, height=400, interpolate='hermite', style=custom_style, dots_size=1)
+    line.title = u'2014年每月下载量统计'
+    line.x_labels = [u'上旬', u'中旬', u'下旬']
+    for month in range(1, 13):
+        line.add(u'2014年%02d月' % month, h.accum(h.days(2014, month)))
+    line.render_to_file(hsvgpath + '2014_day_add.svg')
+
+    line = pygal.Line(
+        width=800, height=400, interpolate='hermite', style=custom_style, dots_size=1)
+    line.title = u'2014年10~11月每日下载量统计'
+    line.x_labels = [u'上旬', u'中旬', u'下旬']
+    line.add(u'2014年10月', h.days(2014, 10))
+    line.add(u'2014年11月', h.days(2014, 11))
+    line.render_to_file(hsvgpath + '2014_11mon.svg')
+
+    line = pygal.Line(
+        width=800, height=400, interpolate='hermite', style=custom_style, dots_size=1)
+    line.title = u'2014年11月最近几周每天下载量统计'
+    line.x_labels = []
+    for hours in range(1, 8):
+        line.x_labels.append(u'周%d' % hours)
+    for week in range(40, 47):
+        line.add(u'2014年 第%d周' % week, h.weekdays(2014, week))
+    line.render_to_file(hsvgpath + '2014_week.svg')
+
+    line = pygal.Line(
+        width=800, height=400, interpolate='hermite', style=custom_style, dots_size=1)
+    line.title = u'2014年11月15~22日每小时下载量统计'
+    line.x_labels = []
+    for hours in range(0, 24):
+        line.x_labels.append(u'%02d点' % hours)
+    for day in range(15, 22):
+        line.add(u'2014年11月%02d日 周%02d' %
+                 (day, day - 14), h.hours(2014, 11, day))
+    line.render_to_file(hsvgpath + '2014_11_hours_add.svg')
+
+
+def torrentsvg():
+    t = torrent.Torrent("../data/torrent.csv")
+    tsvgpath = "../view/torrent/"
+
+    bar = pygal.Bar(width=800, height=400, style=custom_style)
+    bar.title = u'种子一级分类情况统计'
+    for i, num in enumerate(t.topcidnum()):
+        classes = [u'电影',u'剧集',u'音乐',u'动漫',u'游戏',u'综艺',u'体育',u'软件',u'学习',u'其他',u'纪录片']
+        bar.add(classes[i], num)
+    bar.render_to_file(tsvgpath + 'top_class.svg')
+
+    cls = [['dianying.svg',[1000, 1002, 1003, 1004, 1005],[u'电影',u'中国',u'欧美',u'日韩',u'其他']],
+          ['juji.svg',[1200, 1201, 1202, 1203, 1204, 1205, 1206, 1207],[u'剧集', u'大陆', u'港台', u'日剧', u'韩剧', u'欧美', u'合集', u'其他']],
+          ['yinyue.svg',[1400, 1401, 1402, 1403, 1404, 1405, 1406],[u'音乐', u'华语', u'欧美', u'日韩', u'古典', u'MV', u'无损']],
+          ['dongman.svg',[1600, 1601, 1602, 1603, 1604],[u'动漫', u'动画', u'漫画', u'音乐', u'合集']],
+          ['youxi.svg',[1800, 1801, 1802, 1803, 1804],[u'游戏', u'PC', u'PSP', u'其他', u'补丁']],
+          ['zongyi.svg',[2000, 2001, 2002, 2003, 2004],[u'综艺', u'大陆', u'港台', u'日韩', u'其他']],
+          ['tiyu.svg',[2200, 2201, 2202, 2203],[u'体育', u'足球', u'篮球', u'其他']],
+          ['ruanjian.svg',[2400, 2401, 2402, 2403, 2404],[u'软件', u'镜像', u'压缩包', u'exe', u'其他']],
+          ['xuexi.svg',[2600, 2601, 2602, 2603, 2604],[u'学习', u'计算机', u'外语', u'考研', u'课件']],
+          ['qita.svg',[2800, 2801, 2802, 2803, 2804],[u'其他', u'视频', u'音乐', u'图片', u'文档']],
+          ['jilu.svg',[3000, 3001, 3002],[u'纪录片', u'单集', u'合集']]]
+    for cl in cls:
+        bar = pygal.Bar(width=800, height=400, style=custom_style)
+        bar.title = cl[2][0] + u'分类情况统计'
+        for i, num in enumerate(t.cids(cl[1])):
+            classes = cl[2]
+            bar.add(classes[i], num)
+        bar.render_to_file(tsvgpath + cl[0])
+
+if __name__ == '__main__':
+    historysvg()
+    torrentsvg()
